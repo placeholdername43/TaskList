2f9bf577f78195659e5414b48e4b502fa125357c
Finnadip <42492360+Finnadip@users.noreply.github.com>
2024-08-15 08:42:28 +0100

    line chart & pie chart using plotly

--- TaskSite/TaskApp/models.py
+++ TaskSite/TaskApp/models.py
@@ -3,10 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'High'),
+        (2, 'Medium'),
+        (3, 'Low'),
+    ]
+
+
     user_email = models.EmailField()
     task = models.CharField(max_length=255) # can be textfield for large amt of text
     due_by = models.DateTimeField()
-    priority = models.IntegerField(choices=[(1,'High'),(2,'Medium'),(3,'Low')])
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
     is_urgent = models.BooleanField(default=False)
 
     def __str__(self):
--- TaskSite/TaskApp/views.py
+++ TaskSite/TaskApp/views.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
-
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
 from django.shortcuts import redirect, render
 from .models import Task
 from .forms import TaskForm
 
-# Create your views here.
+
 
 def landing_page(request):
     return render(request, 'landing_page.html')
@@ -15,10 +17,36 @@ def dashboard(request):
     tasks_due_in_30d = Task.objects.filter(due_by__lte = next_30_days)
     urgent_tasks = tasks_due_in_30d.filter(is_urgent = True).count()
 
+    line_chart_html = None
+    pie_chart_html = None
+
+    if tasks.exists():
+        df = pd.DataFrame(list(tasks_due_in_30d.values('due_by', 'priority')))
+        df['priority'] = df['priority'].apply(lambda x: dict(Task.PRIORITY_CHOICES).get(x))
+        df['due_by'] = pd.to_datetime(df['due_by']).dt.date
+        tasks_per_day = df.groupby('due_by').size().reset_index(name='count')
+        line_chart_fig = px.line(tasks_per_day, x = "due_by", y = "count", markers = True)
+        line_chart_html = pio.to_html(line_chart_fig, full_html=False)
+
+        tasks_by_priority = df['priority'].value_counts().reset_index(name='count')
+        tasks_by_priority.columns = ['priority','count']
+        pie_chart_fig = px.pie(tasks_by_priority, names = 'priority', values = 'count', title='Tasks by Priority Due in the Next 30 Days')
+        pie_chart_html = pio.to_html(pie_chart_fig, full_html=False)
+    else:
+
+        line_chart_fig = px.line(title = "No tasks due in the next 30 days")
+        line_chart_html = pio.to_html(line_chart_fig, full_html = False)
+
+        pie_chart_fig = px.pie(names=['No data'], values=[1], title = "no tasks by priotiy")
+        pie_chart_html = pio.to_html(pie_chart_fig, full_html=False)
+
+
     taskData = {
         'tasks' : tasks,
         'tasks_due_in_30d' : tasks_due_in_30d,
         'total_urgent_tasks' : urgent_tasks,
+        'line_chart':  line_chart_html,
+        'pie_chart' : pie_chart_html,
 
     }
 
